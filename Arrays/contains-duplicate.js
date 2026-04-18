e4d95ec5605a36e612619068e42d069d20c5cb01
Ayan Mondal <mondalayan01876@gmail.com>
2026-04-18 12:16:20 +0530

    solving  contains-duplicate

---
+++ Arrays/contains-duplicate.js
@@ -0,0 +1,36 @@
+// 217. Contains Duplicate
+// Easy
+// Topics
+// premium lock iconCompanies
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+// Example 1:
+// Input: nums = [1,2,3,1]
+// Output: true
+// Explanation:
+// The element 1 occurs at the indices 0 and 3.
+// Example 2:
+// Input: nums = [1,2,3,4]
+// Output: false
+// Explanation:
+// All elements are distinct.
+// Example 3:
+// Input: nums = [1,1,1,3,3,4,3,2,4,2]
+// Output: true
+
+
+let containsDuplicate = function(nums) {
+    let map = {};
+
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] in map){
+            return true
+        }
+        map[nums[i]] = true
+    }
+    return false
+};
+
+console.log(containsDuplicate([1,2,3,1]))
+console.log(containsDuplicate([1,2,3,4]))
+console.log(containsDuplicate([1,1,1,3,3,4,3,2,4,2]))
