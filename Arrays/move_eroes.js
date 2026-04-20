f71d480f9c060c2f80c141387006d40f8a001f30
Ayan Mondal <mondalayan01876@gmail.com>
2026-04-21 00:39:14 +0530

    Add solution for Move Zeroes problem

---
+++ Arrays/move_eroes.js
@@ -0,0 +1,28 @@
+// Move Zeroes
+// Easy
+// Topics
+// premium lock iconCompanies
+// Hint
+// Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+// Note that you must do this in-place without making a copy of the array.
+
+// Example 1:
+
+// Input: nums = [0,1,0,3,12]
+// Output: [1,3,12,0,0]
+// Example 2:
+// Input: nums = [0]
+// Output: [0]
+const moveZeroes = function(nums) {
+  let right = 0;
+    for (let left=0;left < nums.length; left++){
+        if(nums[left] !== 0){
+            [nums[left], nums[right]] = [nums[right], nums[left]]
+            right++;
+        }
+    }
+};
+
+let test = [0, 0, 1];
+moveZeroes(test);
+console.log(test); // What does this print?
