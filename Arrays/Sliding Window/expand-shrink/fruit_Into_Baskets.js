61b5720864f1433d6883dc906b6e1dd10a7de1d7
Ayan-777 <mondalayan01876@gmail.com>
2026-05-14 18:08:53 +0530

    _________

--- Arrays/Sliding Window/expand-shrink/fruit_Into_Baskets.js
+++ Arrays/Sliding Window/expand-shrink/fruit_Into_Baskets.js
@@ -29,28 +29,28 @@ var totalFruit = function(fruits) {
     let left = 0;    
     let right = 0;     
     let maxLen = 0;  
-    let basket = new Map(); // I renamed 'map' to 'basket' to make it clearer
+    let basket = new Map();
 
     while (right < fruits.length) {
-        // 1. Pick a fruit and put it in the basket
+        
         let currentFruit = fruits[right];
         basket.set(currentFruit, (basket.get(currentFruit) || 0) + 1);
 
-        // 2. If we have more than 2 types of fruit, start shrinking from the left
+       
         while (basket.size > 2) {
             let leftFruit = fruits[left];
             basket.set(leftFruit, basket.get(leftFruit) - 1);
 
-            // 3. If the count of a fruit hits 0, remove it from the basket entirely
+            
             if (basket.get(leftFruit) === 0) {
                 basket.delete(leftFruit);
             }
-            left++; // Move the left side of the window forward
+            left++; 
         }
 
-        // 4. Measure the window and keep the biggest size we've seen
+        
         maxLen = Math.max(maxLen, right - left + 1);
-        right++; // Move the right side of the window forward
+        right++;      
     }
 
     return maxLen;
